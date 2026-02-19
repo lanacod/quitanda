137c08e92e4946139162d286f82ac215ffbd727f
lanacod <puany97@gmail.com>
2026-02-18 21:30:30 -0300

    chore: change api route from frontend

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,10 @@
 const app = require('..');
 
-module.exports = (req, res) => app(req, res);
+module.exports = (req, res) => {
+  if (req.url === '/api') {
+    req.url = '/';
+  } else if (req.url.startsWith('/api/')) {
+    req.url = req.url.replace(/^\/api/, '');
+  }
+  return app(req, res);
+};
